175771e324d9572de1b02b15d2662b687d7480ab
lykismekk <lykismekk@outlook.com>
2021-10-14 15:12:29 +0800

    实现Eureka客户端向Eureka注册中心进行服务注册

--- springcloud-provider-dept-8001/src/main/java/com/kdemo/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/kdemo/springcloud/DeptProvider_8001.java
@@ -2,8 +2,11 @@ package com.kdemo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+// 自动注册服务到服务中心, EnableEurekaClient默认就带上了EnableEureka
+@EnableEurekaClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
--- springcloud-provider-dept-8001/src/main/java/com/kdemo/springcloud/controller/DepartmentController.java
+++ springcloud-provider-dept-8001/src/main/java/com/kdemo/springcloud/controller/DepartmentController.java
@@ -2,7 +2,10 @@ package com.kdemo.springcloud.controller;
 
 import com.kdemo.springcloud.pojo.Department;
 import com.kdemo.springcloud.service.DepartmentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,9 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @PostMapping(path = "/department/add")
     public boolean addDept(Department department) {
         return departmentService.add(department);
@@ -34,4 +40,21 @@ public class DepartmentController {
         return departmentService.findAll();
     }
 
+    // 获取微服务列表清单
+    @GetMapping("/department/discovery")
+    public Object discovery() {
+        // 微服务列表
+        List<String> services = discoveryClient.getServices();
+        System.out.println("discovery=>service" + services);
+        // 得到具体的一个服务,
+        List<ServiceInstance> instances = discoveryClient.getInstances("springcloud-privoider-dept");
+        instances.forEach(x -> {
+            System.out.println(x.getInstanceId()
+                    + "\t" + x.getHost()
+                    + "\t" + x.getPort()
+                    + "\t" + x.getUri()
+                    + "\t" + x.getServiceId());
+        });
+        return this.discoveryClient;
+    }
 }
