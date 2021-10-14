940a5bd5fcd33c8af2c315cfd3a121eecb91dbd1
lykismekk <lykismekk@outlook.com>
2021-10-14 16:16:40 +0800

    修复Idea项目识别问题, 并添加Eureka注册集群

---
+++ springcloud-eureka-7003/src/main/java/com/kdemo/springcloud/Eureka_7003.java
@@ -0,0 +1,14 @@
+package com.kdemo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// 作为Eureka的服务器
+@EnableEurekaServer
+public class Eureka_7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka_7003.class, args);
+    }
+}
