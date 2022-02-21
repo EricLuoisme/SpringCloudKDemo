f09c14bd6964c1628d8f09fef335a744dc358b92
lykismekk <lykismekk@outlook.com>
2022-02-21 15:59:16 +0800

    MyBatis流式查询

--- springcloud-provider-dept-hystrix-8001/src/main/java/com/kdemo/springcloud/dao/DepartmentDao.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/kdemo/springcloud/dao/DepartmentDao.java
@@ -2,6 +2,10 @@ package com.kdemo.springcloud.dao;
 
 import com.kdemo.springcloud.pojo.Department;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.ResultSetType;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +14,15 @@ import java.util.List;
 @Repository
 public interface DepartmentDao {
 
+
     boolean add(Department department);
 
+    /**
+     * MyBatis 流式查询
+     */
+//    @Select("select * from t_iot")
+//    @Options(resultSetType = ResultSetType.FORWARD_ONLY, fetchSize = Integer.MIN_VALUE)
+//    @ResultType(Department.class)
     Department findById(Long departmentId);
 
     List<Department> findAll();
