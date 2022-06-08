eee7003d135b9fb3713e6a1959b995aab2ecaa74
Roylic <roylic@wokoworks.com>
2022-06-08 14:56:46 +0800

    确认即使引入内部静态类, nacos的热加载也会影响重新铸造bean

--- springcloud-provider-nacos-8005/src/main/java/com/kdemo/springcloud/config/ReloadedExtraConfig.java
+++ springcloud-provider-nacos-8005/src/main/java/com/kdemo/springcloud/config/ReloadedExtraConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 
 /**
- * Nacos热加载配置动态配置文件
+ * Nacos热加载配置动态配置文件,
+ * 需要注意, 目前被容器管理的只有ReloadedExtraConfig, 如果
+ * 其内部有一些内部类, 那么内部类的某个属性
  *
  * @author Roylic
  * 2022/5/16
@@ -23,9 +25,20 @@ public class ReloadedExtraConfig {
 
     private String apiPath;
 
+
+    private InsideClass insideClass;
+
+
+    @Data
+    public static class InsideClass {
+        private String simpleUrl;
+    }
+
+
     @PostConstruct
     public void postConstruct() {
         System.out.println(">>> Reload : addressUrl " + addressUrl);
         System.out.println(">>> Reload : apiPath " + apiPath);
+        System.out.println(">>> Reload : simple url " + insideClass.getSimpleUrl());
     }
 }
