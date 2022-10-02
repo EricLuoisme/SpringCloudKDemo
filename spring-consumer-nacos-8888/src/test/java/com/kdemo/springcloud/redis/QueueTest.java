75241bdd88a68f8ac602a2e0af64da42b2ad3c45
roylic <lykismekk@outlook.com>
2022-10-02 15:37:38 +0800

    slightly commit

--- spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/QueueTest.java
+++ spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/QueueTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * Redis 相关测试
