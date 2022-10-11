a60d201ef03ae5381e7dec9d8d04235cf44c6f1c
roylic <lykismekk@outlook.com>
2022-10-11 22:30:05 +0800

    trying bit operations

---
+++ spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/BitTest.java
@@ -0,0 +1,25 @@
+package com.kdemo.springcloud.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Redis 相关测试
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BitTest {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplateObj;
+
+    @Test
+    public void testForBitOps() {
+        redisTemplateObj.opsForValue().setBit("bitTest", 2, true);
+    }
+
+}
