10109ebcbbcdaf4f09b1446cfb65fdb18c275480
roylic <lykismekk@outlook.com>
2022-10-14 00:02:47 +0800

    adding Redisson framework for manipulating the Redis with data-structure of Bloom Filter

---
+++ spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/BloomFilterTest.java
@@ -0,0 +1,26 @@
+package com.kdemo.springcloud.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Redis 相关测试
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BloomFilterTest {
+
+
+    @Test
+    public void bloomFilterTest() {
+
+
+
+    }
+
+
+}
