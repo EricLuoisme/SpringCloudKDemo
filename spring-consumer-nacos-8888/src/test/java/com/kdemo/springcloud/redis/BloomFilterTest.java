844b71732669b7e984c186c26447764b70ffc9a1
roylic <lykismekk@outlook.com>
2022-10-15 16:25:22 +0800

    adding Redisson's bloom filter test

--- spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/BloomFilterTest.java
+++ spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/BloomFilterTest.java
@@ -2,9 +2,10 @@ package com.kdemo.springcloud.redis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.redisson.Redisson;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -18,8 +19,24 @@ public class BloomFilterTest {
     @Test
     public void bloomFilterTest() {
 
+        RedissonClient redissonClient = Redisson.create();
 
+        RBloomFilter<Object> bloomFilterTest = redissonClient.getBloomFilter("BloomFilterTest");
+        bloomFilterTest.tryInit(1000L, 0.03);
 
+        bloomFilterTest.add("a");
+        bloomFilterTest.add("b");
+        bloomFilterTest.add("c");
+        bloomFilterTest.add("d");
+
+        bloomFilterTest.getExpectedInsertions();
+        bloomFilterTest.getFalseProbability();
+        bloomFilterTest.getHashIterations();
+
+        bloomFilterTest.contains("a");
+        bloomFilterTest.count();
+
+        redissonClient.shutdown();
     }
 
 
