237de482353720bc7caff4cc80c23559def32ffa
roylic <lykismekk@outlook.com>
2022-10-22 14:59:22 +0800

    adding configuration for redis template

---
+++ spring-consumer-nacos-8888/src/main/java/com/kdemo/springcloud/config/ReactiveTemplateManager.java
@@ -0,0 +1,27 @@
+package com.kdemo.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ReactiveTemplateManager {
+
+    @Bean
+    public ReactiveRedisConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.fromSerializer(
+                new StringRedisSerializer()));
+    }
+
+
+}
--- spring-consumer-nacos-8888/src/main/java/com/kdemo/springcloud/config/RedisCacheManager.java
+++ spring-consumer-nacos-8888/src/main/java/com/kdemo/springcloud/config/RedisCacheManager.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.redisson.client.protocol.decoder.ObjectMapDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
