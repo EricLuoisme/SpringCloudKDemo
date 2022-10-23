35f599989e526329ff30880f2d47abd51b2a7a32
roylic <lykismekk@outlook.com>
2022-10-23 17:28:44 +0800

    adding Redis Stream with synchronous test

--- spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/StreamTest.java
+++ spring-consumer-nacos-8888/src/test/java/com/kdemo/springcloud/redis/StreamTest.java
@@ -7,14 +7,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.stream.ByteRecord;
-import org.springframework.data.redis.connection.stream.StreamRecords;
-import org.springframework.data.redis.connection.stream.StringRecord;
+import org.springframework.data.redis.connection.stream.*;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -53,6 +52,40 @@ public class StreamTest {
         // 2. append the msg through redis-template
         StringRecord record_str = StreamRecords.string(msgMap_Str).withStreamKey("Stream_1");
         redisTemplateObj.opsForStream().add(record_str);
+    }
+
+
+    @Test
+    public void consumingMsgSync() {
+        // block until stream is not empty
+
+        // a. by creating Consumer Group for read
+//        List<MapRecord<String, Object, Object>> read = redisTemplateObj.opsForStream().read(
+//                Consumer.from("Sync-Consumer", "C-1"),
+//                StreamReadOptions.empty().count(2),
+//                StreamOffset.create("Stream_1", ReadOffset.lastConsumed()));
+
+        // b. just read without Consumer Group
+        List<MapRecord<String, Object, Object>> read = redisTemplateObj.opsForStream().read(
+                StreamReadOptions.empty().count(10),
+                StreamOffset.fromStart("Stream_1"));
+
+        // read
+        MapRecord<String, Object, Object> entries = read.get(0);
+        Map<Object, Object> value = entries.getValue();
+        for (Map.Entry<Object, Object> entry : value.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println();
+        }
+    }
+
+
+    @Test
+    public void consumingMsgASync() {
+        // For asynchronous handling, should implement by:
+        // - StreamMessageListenerContainer
+        // - StreamReceiver
     }
 
 }
