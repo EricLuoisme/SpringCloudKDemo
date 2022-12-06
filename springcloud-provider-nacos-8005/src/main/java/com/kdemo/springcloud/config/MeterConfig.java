f1974c8f40f43a226bfbca6a4714ce656600908e
roylic <lykismekk@outlook.com>
2022-12-07 00:05:15 +0800

    adding Loki-Promtail test

---
+++ springcloud-provider-nacos-8005/src/main/java/com/kdemo/springcloud/config/MeterConfig.java
@@ -0,0 +1,17 @@
+package com.kdemo.springcloud.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MeterConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer(@Value("${spring.application.name}") String appName) {
+        return registry -> registry.config().commonTags("application", appName);
+    }
+
+}
