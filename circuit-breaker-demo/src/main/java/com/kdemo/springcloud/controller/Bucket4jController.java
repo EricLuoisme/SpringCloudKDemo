d82c75cc0d4c263d9b24406fad1e786ea3ab51d7
roylic <roylic@wokoworks.com>
2023-02-02 17:10:59 +0800

    slight comment edition on Bucket4j

--- circuit-breaker-demo/src/main/java/com/kdemo/springcloud/controller/Bucket4jController.java
+++ circuit-breaker-demo/src/main/java/com/kdemo/springcloud/controller/Bucket4jController.java
@@ -13,26 +13,33 @@ import java.time.Duration;
 @RequestMapping("/bucket4j")
 public class Bucket4jController {
 
-
+    // Bucket interface represents the token bucket with a maximum capacity
     private final Bucket bucket;
 
 
     public Bucket4jController() {
         this.bucket = Bucket.builder()
                 .addLimit(
+                        // Bandwidth defines the limits of the bucket -> capacity & rate to refill
                         Bandwidth.classic(5,
+                                // Refill is used to define the fixed rate at which tokens are added to the bucket
                                 Refill.greedy(60, Duration.ofMinutes(1)))
                 )
                 .build();
     }
 
 
-    @PostMapping("/test")
-    public String test_Endpoint() {
+    @PostMapping("/tryConsume")
+    public String tryConsume_Endpoint() {
         if (bucket.tryConsume(1)) {
             return "okk";
         }
         return "try again";
+    }
+
+    @PostMapping("/tryConsumeAndRemaining")
+    public String tryConsumeAndRemaining_Endpoint() {
+        return bucket.tryConsumeAndReturnRemaining(1).toString();
     }
 
 
