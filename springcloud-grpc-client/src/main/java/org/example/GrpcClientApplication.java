0fa7d3cb49228fb466b7b386f331304091c14d8a
roylic <roylic@wokoworks.com>
2023-04-12 14:37:58 +0800

    correct the service and make mvp

--- springcloud-grpc-client/src/main/java/org/example/GrpcClientApplication.java
+++ springcloud-grpc-client/src/main/java/org/example/GrpcClientApplication.java
@@ -2,16 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Roylic
  * 2023/4/12
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class GrpcServerApplication {
+public class GrpcClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GrpcServerApplication.class, args);
+        SpringApplication.run(GrpcClientApplication.class, args);
     }
 }
