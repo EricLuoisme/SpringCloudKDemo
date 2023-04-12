1e3b045090e67f63069fe9a85df87d83678b7759
roylic <roylic@wokoworks.com>
2023-04-12 14:01:14 +0800

    adding interface client server for grpc

---
+++ springcloud-grpc-server/src/main/java/org/example/GrpcServerApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Roylic
+ * 2023/4/12
+ */
+@SpringBootApplication
+public class GrpcServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+}
