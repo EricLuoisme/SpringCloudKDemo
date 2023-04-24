a27f3191bde83ae8dd9f5ebde9d636426116bcb2
roylic <lykismekk@outlook.com>
2023-04-24 22:44:40 +0800

    make sure starter working

--- spring-consumer-nacos-8888/src/main/java/com/kdemo/springcloud/NacosConsumer_8888.java
+++ spring-consumer-nacos-8888/src/main/java/com/kdemo/springcloud/NacosConsumer_8888.java
@@ -3,9 +3,11 @@ package com.kdemo.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.kdemo.springcloud"})
 public class NacosConsumer_8888 {
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumer_8888.class, args);
