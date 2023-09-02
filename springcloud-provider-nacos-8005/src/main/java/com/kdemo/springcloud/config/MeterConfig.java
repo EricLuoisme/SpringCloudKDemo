51c1cee0ca83e88239537a99cee8a5daf091c67b
roylic <lykismekk@outlook.com>
2023-09-02 18:14:08 +0800

    try to add metric info to prometheus, but prometheus not recognized it

--- springcloud-provider-nacos-8005/src/main/java/com/kdemo/springcloud/config/MeterConfig.java
+++ springcloud-provider-nacos-8005/src/main/java/com/kdemo/springcloud/config/MeterConfig.java
@@ -1,17 +1,23 @@
-//package com.kdemo.springcloud.config;
-//
-//import io.micrometer.core.instrument.MeterRegistry;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class MeterConfig {
-//
+package com.kdemo.springcloud.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MeterConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer(@Value("${spring.application.name}") String appName) {
+        return registry -> registry.config().commonTags("application", appName);
+    }
+
 //    @Bean
-//    public MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer(@Value("${spring.application.name}") String appName) {
-//        return registry -> registry.config().commonTags("application", appName);
+//    public MeterRegistry getMeterRegistry() {
+//        return new CompositeMeterRegistry();
 //    }
-//
-//}
+
+}
