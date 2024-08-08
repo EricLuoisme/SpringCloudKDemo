2eac1efa6f6f37cccdfc4fdba354548078ac8de5
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-08-08 23:31:34 +0800

    feat: adding dubbo v2 project structure for further classloader issue

---
+++ spring-cloud-dubbo-v2/dubbo-v2-producer/src/main/java/com/own/dubbo2/producer/DubboV2Application.java
@@ -0,0 +1,10 @@
+package com.own.dubbo2.producer;
+
+
+@EnableDubbo
+@SpringBootApplication
+public class DubboV2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboV2Application.class, args);
+    }
+}
