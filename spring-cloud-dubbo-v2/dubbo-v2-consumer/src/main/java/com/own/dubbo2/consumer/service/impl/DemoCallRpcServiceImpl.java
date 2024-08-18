31e2377298307cb1dd8125cab4606ddd404296b1
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-08-18 23:38:15 +0800

    chore: tmp cmt, problem is not related to ip it got, might related to provider could not receive any calling

--- spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/service/impl/DemoCallRpcServiceImpl.java
+++ spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/service/impl/DemoCallRpcServiceImpl.java
@@ -1,38 +1,38 @@
-package com.own.dubbo2.consumer.service.impl;
-
-import com.own.dubbo2.DemoCallRpcService;
-import com.own.dubbo2.DemoRpcService;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.dubbo.config.annotation.DubboReference;
-import org.apache.dubbo.config.annotation.DubboService;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-
-@Slf4j
-@DubboService
-public class DemoCallRpcServiceImpl implements DemoCallRpcService {
-
-    @DubboReference
-    DemoRpcService demoRpcService;
-
-    @Override
-    public String howToSayHelloAsync(String name) {
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(
-                () -> demoRpcService.sayHello(name)
-        );
-
-        try {
-            TimeUnit.SECONDS.sleep(10);
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public String howToSayHelloSync(String name) {
-        return demoRpcService.sayHello(name);
-    }
-}
+//package com.own.dubbo2.consumer.service.impl;
+//
+//import com.own.dubbo2.DemoCallRpcService;
+//import com.own.dubbo2.DemoRpcService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.dubbo.config.annotation.DubboReference;
+//import org.apache.dubbo.config.annotation.DubboService;
+//
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ExecutionException;
+//import java.util.concurrent.TimeUnit;
+//
+//@Slf4j
+//@DubboService
+//public class DemoCallRpcServiceImpl implements DemoCallRpcService {
+//
+//    @DubboReference
+//    DemoRpcService demoRpcService;
+//
+//    @Override
+//    public String howToSayHelloAsync(String name) {
+//        CompletableFuture<String> future = CompletableFuture.supplyAsync(
+//                () -> demoRpcService.sayHello(name)
+//        );
+//
+//        try {
+//            TimeUnit.SECONDS.sleep(10);
+//            return future.get();
+//        } catch (InterruptedException | ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @Override
+//    public String howToSayHelloSync(String name) {
+//        return demoRpcService.sayHello(name);
+//    }
+//}
