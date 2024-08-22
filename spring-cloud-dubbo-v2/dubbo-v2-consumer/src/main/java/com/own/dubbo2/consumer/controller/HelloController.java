267d15cc160dcf3e97f4066be8f72f533ff63013
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-08-22 23:18:33 +0800

    feat: add parallel stream to check whether it would trigger same issue or not (inner also use common ForkJoinPool)

--- spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/controller/HelloController.java
+++ spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/controller/HelloController.java
@@ -5,7 +5,9 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @RestController
 public class HelloController {
@@ -25,5 +27,14 @@ public class HelloController {
         return CompletableFuture.supplyAsync(
                         () -> demoRpcService.sayHello("HelloAsync"))
                 .thenApply(str -> str + "\n");
+    }
+
+    @GetMapping("/parallel")
+    public String helloParallel() {
+        Optional<String> opHello = Arrays.asList(1, 2).stream()
+                .parallel()
+                .map(x -> demoRpcService.sayHello("HelloParallel" + x))
+                .findFirst();
+        return opHello.get();
     }
 }
