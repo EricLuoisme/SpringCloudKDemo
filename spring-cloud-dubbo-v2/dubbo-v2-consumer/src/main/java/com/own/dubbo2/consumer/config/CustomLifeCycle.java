9b7096d33ef4325fb028f0402ade07d8f338d467
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-08-24 14:55:36 +0800

    chore: by using SmartLifeCycle, or listen the ApplicationReadyEvent, they all use Main thread to handle the event callback, which use the same: AppClassLoader

--- spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/config/CustomLifeCycle.java
+++ spring-cloud-dubbo-v2/dubbo-v2-consumer/src/main/java/com/own/dubbo2/consumer/config/CustomLifeCycle.java
@@ -1,40 +1,38 @@
-package com.own.dubbo2.consumer.config;
-
-import lombok.extern.slf4j.Slf4j;
-
-import org.apache.dubbo.common.extension.ExtensionLoader;
-import org.apache.dubbo.rpc.cluster.LoadBalance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.SmartLifecycle;
-import org.springframework.context.annotation.Configuration;
-
-@Slf4j
-@Configuration
-public class CustomLifeCycle implements SmartLifecycle {
-
-    @Override
-    public void start() {
-
+//package com.own.dubbo2.consumer.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+//import org.apache.dubbo.common.extension.ExtensionLoader;
+//import org.apache.dubbo.rpc.cluster.LoadBalance;
+//import org.springframework.context.SmartLifecycle;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Slf4j
+//@Configuration
+//public class CustomLifeCycle implements SmartLifecycle {
+//
+//    @Override
+//    public void start() {
+//
 //        Thread thread = Thread.currentThread();
 //        log.info("[CustomSmartLifeCycle] current thread:{}, thread context class-loader{}",
 //                thread.getName(), thread.getContextClassLoader());
-
-        // load dubbo config just after start, eliminate the forkJoinPool's
-        // thread with different class loader cause issue
+//
+//        // load dubbo config just after start, eliminate the forkJoinPool's
+//        // thread with different class loader cause issue
 //        log.debug("[CustomSmartLifeCycle] preloading start, thread name:{}, thread context classloader:{}",
 //                thread.getName(), thread.getContextClassLoader());
 //        ExtensionLoader.getExtensionLoader(LoadBalance.class)
 //                .getDefaultExtensionName();
 //        log.debug("[CustomSmartLifeCycle] preloading finished");
-    }
-
-    @Override
-    public void stop() {
-    }
-
-    @Override
-    public boolean isRunning() {
-        return false;
-    }
-}
+//    }
+//
+//    @Override
+//    public void stop() {
+//    }
+//
+//    @Override
+//    public boolean isRunning() {
+//        return false;
+//    }
+//}
