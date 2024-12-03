5495614759071c8cc76435b5896275b7239ce383
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-12-03 22:34:55 +0800

    feat: finished the whole ZSet feeding logic with auto-spin impl for rearrange ZSet size & update ttl expiration time

--- spring-consumer-nacos-8887/src/main/java/com/kdemo/springcloud/helper/RankingHelper.java
+++ spring-consumer-nacos-8887/src/main/java/com/kdemo/springcloud/helper/RankingHelper.java
@@ -1,5 +1,6 @@
 package com.kdemo.springcloud.helper;
 
+import com.kdemo.springcloud.score.ScoreConvertor;
 import com.kdemo.springcloud.score.convertor.FIRFConvertor;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Collections;
 
+import static com.kdemo.springcloud.scripts.LuaBasicOps.EXPIRATION_SCRIPT;
+import static com.kdemo.springcloud.scripts.LuaZSetOps.REMOVE_MEMBER_OUT_RANGE_SCRIPT;
 import static com.kdemo.springcloud.scripts.LuaZSetOps.Z_ADD_GT_SCRIPT;
 
 /**
@@ -21,7 +24,20 @@ import static com.kdemo.springcloud.scripts.LuaZSetOps.Z_ADD_GT_SCRIPT;
 @AllArgsConstructor
 public class RankingHelper {
 
-    private static final FIRFConvertor CONVERTOR = new FIRFConvertor();
+    // Auto spin configs
+    private static Integer BOARD_CALLING_COUNTS = 0;
+
+    private static Integer HASH_CALLING_COUNTS = 0;
+
+    private static final Integer REARRANGE_THRESHOLD = 500;
+
+    private static final Integer EXPIRE_THRESHOLD = 500;
+
+    private static final Integer TRIGGER_MARGIN = Integer.MAX_VALUE - 3000;
+
+
+    // score convertor
+    private static final ScoreConvertor CONVERTOR = new FIRFConvertor();
 
     private final RedissonClient redissonClient;
 
@@ -29,30 +45,61 @@ public class RankingHelper {
     /**
      * TODO combine ops of 1) update leaderboard & 2) update user own score
      */
-    public void uploadScore(String boardKey, long baseTime, String unqId, Long gameScore) {
+    public void uploadScore(String boardKey, int boardSize, long baseTime, long expireTime,
+                            String unqId, Long gameScore) {
         // TODO
-        updateLeaderboard(redissonClient, boardKey, unqId, gameScore, baseTime);
+        updateLeaderboard(redissonClient, boardKey, boardSize, baseTime, expireTime, unqId, gameScore);
 
         // TODO
         updateUserScore();
     }
 
-    private static boolean updateLeaderboard(RedissonClient redissonClient, String boardKey, String unqId, Long gameScore, Long baseTimestamp) {
+    /**
+     * Update Redis ZSet as leaderboard
+     *
+     * @param redissonClient redisson client
+     * @param boardKey       zSet key
+     * @param boardSize      zSet remaining size
+     * @param baseTime       base time (for score)
+     * @param expireTime     expire time (for zSet)
+     * @param unqId          member
+     * @param gameScore      raw score value
+     * @return update board raw Redis result
+     */
+    private static String updateLeaderboard(RedissonClient redissonClient, String boardKey, int boardSize,
+                                            Long baseTime, Long expireTime, String unqId, Long gameScore) {
 
         // score conversion
         double cacheScore = CONVERTOR.convertToZSetScore(
-                gameScore, Instant.now().toEpochMilli() - baseTimestamp);
+                gameScore, Instant.now().toEpochMilli() - baseTime);
 
         // Lua script execution
         Object rawResult = redissonClient.getScript(StringCodec.INSTANCE).eval(
                 RScript.Mode.READ_WRITE, Z_ADD_GT_SCRIPT, RScript.ReturnType.INTEGER,
                 Collections.singletonList(boardKey), unqId, Double.toString(cacheScore));
 
-        // TODO board size cutting
+        // board size cutting -> instead of triggering each time, only reach specific calling times would executed
+        if (BOARD_CALLING_COUNTS % REARRANGE_THRESHOLD == 0) {
+            Object rearrangeResult = redissonClient.getScript(StringCodec.INSTANCE).eval(
+                    RScript.Mode.READ_WRITE, REMOVE_MEMBER_OUT_RANGE_SCRIPT, RScript.ReturnType.INTEGER,
+                    Collections.singletonList(boardKey), boardSize);
+            log.info("ZSet Re-arrange triggered with result:{}", rearrangeResult);
+        }
 
-        // TODO expiration
+        // expiration setting -> instead of triggering each time, only reach specific calling times would executed
+        if (BOARD_CALLING_COUNTS == 0 || BOARD_CALLING_COUNTS % EXPIRE_THRESHOLD == 0) {
+            Object ttlResult = redissonClient.getScript(StringCodec.INSTANCE)
+                    .eval(RScript.Mode.READ_WRITE, EXPIRATION_SCRIPT,
+                            RScript.ReturnType.INTEGER, Collections.singletonList(boardKey), expireTime);
+            log.info("ZSet set TTL triggered with result:{}", ttlResult);
+        }
 
-        return true;
+        // reset counting
+        if (BOARD_CALLING_COUNTS > TRIGGER_MARGIN) {
+            BOARD_CALLING_COUNTS = 0;
+        }
+
+        return String.valueOf(rawResult);
     }
 
     private static boolean updateUserScore() {
